24221cfebbff6407c98342896bfa5a80f8ef4df8
Ccordova41 <ccordova41@gmail.com>
2018-02-21 14:39:45 -0500

    formatted body

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,8 +4,11 @@ const Body = () => {
   return(
     <div className="main-content-body">
       <p>This one is for all the WHOVIANS who need a little more Doctor in their lives!<br />
-      Here you will find your self surrounded by images of the Doctor (both new and past), his adventures, and the companions that have shaped his hearts.<br />
-      Click on the Weeping Angel below; but please do remember to never blink...
+      Here you will find yourself surrounded by images of the Doctor (both new and past), his adventures, and the companions that have shaped his hearts.<br /><br />
+      Click on the Tardis below to get started! <br />
+      <a href="/">
+      <img border="0" alt="Tardis" src="tardis.png" width="156" height="242" />
+      </a>
      </p>
     </div>
   )
