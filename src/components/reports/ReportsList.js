6f9339881b8d8725e13ae0122631bfb5bcee8e26
Ccordova41 <ccordova41@gmail.com>
2018-02-21 21:27:36 -0500

    list all comments added

---
+++ src/components/reports/ReportsList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import Report from './Report'
+
+
+class ReportsList extends Component {
+
+  render() {
+
+    const associatedReports = this.props.reportData.filter(report => report.doctor_id === this.props.doctorId)
+    const reports = associatedReports.map((report) => {
+      return <Report report={report}/>
+    })
+
+    if (reports) {
+      return(
+        <div className="reports">
+          <ul>
+            {reports}
+          </ul>
+        </div>
+      )
+    }
+  }
+
+}
+
+function mapStateToProps(state) {
+  return {
+    reportData: state.reports
+  }
+}
+
+export default connect(mapStateToProps)(ReportsList)
