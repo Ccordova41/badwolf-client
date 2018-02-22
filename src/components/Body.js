7f13969e293a7700c5e20bf5e3ca8df8dc4e0268
Ccordova41 <ccordova41@gmail.com>
2018-02-21 19:15:09 -0500

    formatting css

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,7 +7,7 @@ const Body = () => {
       Here you will find yourself surrounded by images of the Doctor (both new and past), his adventures, and the companions that have shaped his hearts.<br /><br />
       Click on the Tardis below to get started! <br />
       <a href="/doctors">
-      <img border="0" alt="Tardis" src="tardis.png" width="156" height="242" />
+      <img border="0" alt="Tardis" src="tardis1.gif" width="200" height="286" />
       </a>
      </p>
     </div>
