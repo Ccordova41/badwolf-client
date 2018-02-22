819b35642d2146b2a5f60b96086548092619f27f
Ccordova41 <ccordova41@gmail.com>
2018-02-21 21:24:55 -0500

    show report

--- src/components/doctors/DoctorsShow.js
+++ src/components/doctors/DoctorsShow.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import ReportForm from '../reports/ReportForm';
+import { addToSelect } from '../../actions/doctorAction'
 
 class DoctorsShow extends Component {
 
+  handleOnClick = () => {
+    const selectDoctor = {
+      select: true,
+      doctorId: this.props.doctor.id
+    }
+    this.props.addToSelect(selectDoctor);
+  }
+
+  onUnload = (event) => {
+    event.returnValue = "Please don't reload!"
+  }
+
+  componentDidMount = () => {
+    window.addEventListener("beforeunload", this.onUnload)
+  }
+
+  componentWillUnmount = () => {
+    window.removeEventListener("beforeunload", this.onUnload)
+  }
 
 
   render() {
@@ -13,6 +34,11 @@ class DoctorsShow extends Component {
         <div className="doctor-gif">
           <img src={this.props.doctor.images.fixed_height.url} />
         </div>
+        <button onClick={this.handleOnClick}>Select this character!</button>
+        <div className="reports-show">
+        <CommentForm doctorId={this.props.doctor.id}/>
+
+
       </div>
     )
   }
